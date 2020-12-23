b242f8d4698b9407c81faa7c0843bee394c1c386
dbandstra <dbandstra@protonmail.com>
2020-12-23 14:26:21 -0800

    fix some typos

--- generated/webgl.zig
+++ generated/webgl.zig
@@ -174,26 +174,28 @@ pub fn glShaderSource(
 }
 
 fn getBytesPerPixel(format: GLenum, type_: GLenum) GLsizei {
-    switch (type_) {
-        GL_UNSIGNED_BYTE => {
-            switch (format) {
-                GL_RGBA => return 4,
-                GL_RGB => return 3,
-                GL_LUMINANCE_ALPHA => return 2,
-                GL_LUMINANCE, GL_ALPHA => return 1,
-            }
+    return switch (type_) {
+        GL_UNSIGNED_BYTE => switch (format) {
+            GL_RGBA => 4,
+            GL_RGB => 3,
+            GL_LUMINANCE_ALPHA => 2,
+            GL_LUMINANCE, GL_ALPHA => 1,
+            else => 0,
         },
-        GL_UNSIGNED_SHORT_4_4_4_4 => {
-            if (format == gl.RGBA) return 2;
+        GL_UNSIGNED_SHORT_4_4_4_4 => switch (format) {
+            GL_RGBA => 2,
+            else => 0,
         },
-        GL_UNSIGNED_SHORT_5_5_5_1 => {
-            if (format == gl.RGBA) return 2;
+        GL_UNSIGNED_SHORT_5_5_5_1 => switch (format) {
+            GL_RGBA => 2,
+            else => 0,
         },
-        GL_UNSIGNED_SHORT_5_6_5 => {
-            if (format == gl.RGB) return 2;
+        GL_UNSIGNED_SHORT_5_6_5 => switch (format) {
+            GL_RGB => 2,
+            else => 0,
         },
-    }
-    return 0; // invalid
+        else => 0,
+    };
 }
 
 pub fn glTexImage2D(
@@ -215,7 +217,7 @@ pub fn glTexImage2D(
 
         const pixels_len = width * height * bytes_per_pixel;
 
-        glTexImage2D_api(target, level, internal_format, width, height, border, format, type_, pixels, pixels_len);
+        glTexImage2D_api(target, level, internalformat, width, height, border, format, type_, pixels, pixels_len);
     }
 }
 
